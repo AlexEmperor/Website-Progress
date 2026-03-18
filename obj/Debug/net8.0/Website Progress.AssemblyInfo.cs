4649fbe4bc4ee9ef8fdb7d3d1609cb0f2175bfbd
Alex_Zolotukhin <alex04120445@mail.ru>
2026-03-18 13:36:41 +0300

    fix news update pages

--- obj/Debug/net8.0/Website Progress.AssemblyInfo.cs
+++ obj/Debug/net8.0/Website Progress.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Website Progress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95f06366d4cfb7b64fad59faab54388b6e98777a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb20fea13e8d02108510c83e70756bf3bf287f05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Website Progress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Website Progress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
